26de0714f58dd70e43f801fa74fb1c9abb777819
Sergei Datsenko <dats@chromium.org>
2017-08-30 10:46:24 +1000

    [HACK] Implement getting current time in NodePlatform
    It's required by an upcoming change in V8:
    https://chromium-review.googlesource.com/c/v8/v8/+/598666

--- src/node_platform.h
+++ src/node_platform.h
@@ -52,6 +52,7 @@ class NodePlatform : public v8::Platform {
                                      double delay_in_seconds) override;
   bool IdleTasksEnabled(v8::Isolate* isolate) override;
   double MonotonicallyIncreasingTime() override;
+  double CurrentClockTimeMillis() override;
   v8::TracingController* GetTracingController() override;
 
  private:
