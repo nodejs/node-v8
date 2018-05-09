5d01852d6abcc1894c0cccd73f463d9cbf9732f7
Gabriel Charette <gab@chromium.org>
2018-05-09 12:20:04 -0400

    src: use modern v8::Platform worker threads APIs
    Precursor to removing deprecated APIs on the v8 side @
    https://chromium-review.googlesource.com/c/v8/v8/+/1045310

--- src/node.cc
+++ src/node.cc
@@ -307,7 +307,7 @@ static struct {
   }
 
   void DrainVMTasks(Isolate* isolate) {
-    platform_->DrainBackgroundTasks(isolate);
+    platform_->DrainTasks(isolate);
   }
 
   void CancelVMTasks(Isolate* isolate) {
