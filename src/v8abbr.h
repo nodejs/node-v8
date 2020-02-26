ristmp00/node

src: update v8abbr.h for V8 update
This commit updates v8abbr.h to use the updated metadata.

src/v8abbr.h
@@ -105,9 +105,9 @@
 #define V8_OFF_STR_CHARS  \
     V8_OFF_HEAP(V8DBG_CLASS_SEQONEBYTESTRING__CHARS__CHAR)
 #define V8_OFF_CONSSTR_CAR  \
-    V8_OFF_HEAP(V8DBG_CLASS_CONSSTRING__FIRST_OFFSET__INT)
+    V8_OFF_HEAP(V8DBG_CLASS_CONSSTRING__FIRST_STRING)
 #define V8_OFF_CONSSTR_CDR  \
-    V8_OFF_HEAP(V8DBG_CLASS_CONSSTRING__SECOND_OFFSET__INT)
+    V8_OFF_HEAP(V8DBG_CLASS_CONSSTRING__SECOND_STRING)
 #define V8_OFF_EXTSTR_RSRC  \
     V8_OFF_HEAP(V8DBG_CLASS_EXTERNALSTRING__RESOURCE__OBJECT)
 #define V8_OFF_FA_SIZE    \

nodejs/node-v8

src/v8abbr.h
@@ -105,9 +105,9 @@
 #define V8_OFF_STR_CHARS  \
     V8_OFF_HEAP(V8DBG_CLASS_SEQONEBYTESTRING__CHARS__CHAR)
 #define V8_OFF_CONSSTR_CAR  \
-    V8_OFF_HEAP(V8DBG_CLASS_CONSSTRING__FIRST_OFFSET__INT)
+    V8_OFF_HEAP(V8DBG_CLASS_CONSSTRING__FIRST__STRING)
 #define V8_OFF_CONSSTR_CDR  \
-    V8_OFF_HEAP(V8DBG_CLASS_CONSSTRING__SECOND_OFFSET__INT)
+    V8_OFF_HEAP(V8DBG_CLASS_CONSSTRING__SECOND__STRING)
 #define V8_OFF_EXTSTR_RSRC  \
     V8_OFF_HEAP(V8DBG_CLASS_EXTERNALSTRING__RESOURCE__OBJECT)
 #define V8_OFF_FA_SIZE    \