3e6ec7785fb6c29cdc85baa2191ebf73857a8756
Michaël Zasso <targos@protonmail.com>
2021-04-11 11:03:25 +0200

    chore: create GitHub action to update the canary branch (#171)

---
+++ README.md
@@ -0,0 +1,17 @@
+# Node.js V8 canary
+
+This is an automatically updated **experimental** version of [Node.js][] with
+the `lkgr` (last known good revision) of V8.
+
+The daily builds of this repo can be found at [`v8-canary`][].
+
+**Do not use this in production!**
+
+This repository is not owned by `@nodejs/v8`, but they might be able to help
+with issues.
+
+This project is bound by a [Code of Conduct][].
+
+[Code of Conduct]: https://github.com/nodejs/admin/blob/HEAD/CODE_OF_CONDUCT.md
+[Node.js]: https://github.com/nodejs/node
+[`v8-canary`]: https://nodejs.org/download/v8-canary/
---
+++ tools/update-canary.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -ex
+
+cd node-v8
+
+git remote add upstream https://github.com/nodejs/node.git
+git fetch upstream master
+git fetch upstream canary-base
+
+git config user.name "Node.js GitHub Bot"
+git config user.email github-bot@iojs.org
+
+git reset --hard upstream/master
+
+# Update V8 to the lkgr branch
+git-node v8 major --branch=lkgr --base-dir="$GITHUB_WORKSPACE"
+
+# Cherry-pick the floating V8 patches Node.js maintains on master.
+# Canary-base is the last good version of canary, and is manually updated with any V8 patches or backports.
+git cherry-pick `git log upstream/canary-base -1 --format=format:%H --grep "src: update NODE_MODULE_VERSION"`...upstream/canary-base
+
+# Verify that Node.js can be compiled and executed
+python3 ./configure
+make -j $(getconf _NPROCESSORS_ONLN) V=
+out/Release/node test/parallel/test-process-versions.js
+
+# Force-push to the canary branch.
+git push --force origin HEAD:canary
